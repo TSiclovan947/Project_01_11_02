2a4d98eb306938fbe5a9a22daff3729b3ae63296
Unknown <tsiclovan947@west-mec.org>
2018-08-31 18:27:49 -0700

    XHR Object
    Created a global variable; Created a function called getRequestObject with a try and catch to create an XHR object; Created a function to stop the default submission from executing; Function to request stock quote data; Created event handlers and began to generate AJAX request

--- script.js
+++ script.js
@@ -7,3 +7,67 @@
 */
 
 "use strict";
+
+//Global variable
+var httpRequest = false;
+var entry = "^IXIC";
+
+//Function to Create XHR Object
+function getRequestObject() {
+    try {
+        httpRequest = new XMLHttpRequest(); //XHR Object
+    }
+    catch (requestError) {
+        return false;
+    }
+    //alert(httpRequest);
+    return httpRequest;
+}
+
+//Function to stop default submission from executing
+function stopSubmission(evt) {
+    //alert("stopSubmission()");
+    if (evt.preventDefault) {
+        evt.preventDefault();
+    }
+    else {
+        evt.returnValue = false;
+    }
+    getQuote();
+}
+
+//Funtion to request stock quote data
+function getQuote() {
+    //alert("getQuote()");
+    if (document.getElementsByTagName("input")[0].value) {
+        entry = document.getElementsByTagName("input")[0].value;
+    }
+    if (!httpRequest) {
+        httpRequest = getRequestObject();
+    }
+    //Begin to generate AJAX request
+    httpRequest.abort();
+    httpRequest.open("get", "StockCheck.php?t=" + entry, true);
+    httpRequest.send(null);
+    //Event handler for the onreadystatechange
+    httpRequest.onreadystatechange = displayData;
+}
+
+//Event handler to call function on subit event
+var form = document.getElementsByTagName("form")[0];
+if (form.addEventListener) {
+    form.addEventListener("submit", stopSubmission, false);
+    window.addEventListener("load", getQuote);
+}
+else if (form.attachEvent) {
+    form.attachEvent("onsubmit", stopSubmission);
+    window.attachEvent("onload", getQuote);
+}
+
+//Event handler for the page load event
+if (window.addEventListener) {
+    window.addEventListener("load", getRequestObject, false);
+}
+else if (window.attachEvent) {
+    window.attachEvent("onload", getRequestObject);
+}

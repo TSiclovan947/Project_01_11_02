b9c69307180379f52ad5b514a054e23c2d36f503
Unknown <tsiclovan947@west-mec.org>
2018-09-04 17:03:21 -0700

    Final Code Commit
    Created a function event handler to see if the data is given back; Created a function to get better styles into the stock data (formatTable); Added a setTimeout to refresh the code sequence every ten seconds

--- script.js
+++ script.js
@@ -16,8 +16,7 @@ var entry = "^IXIC";
 function getRequestObject() {
     try {
         httpRequest = new XMLHttpRequest(); //XHR Object
-    }
-    catch (requestError) {
+    } catch (requestError) {
         return false;
     }
     //alert(httpRequest);
@@ -29,8 +28,7 @@ function stopSubmission(evt) {
     //alert("stopSubmission()");
     if (evt.preventDefault) {
         evt.preventDefault();
-    }
-    else {
+    } else {
         evt.returnValue = false;
     }
     getQuote();
@@ -38,6 +36,7 @@ function stopSubmission(evt) {
 
 //Funtion to request stock quote data
 function getQuote() {
+    //console.log("getQuote()");
     //alert("getQuote()");
     if (document.getElementsByTagName("input")[0].value) {
         entry = document.getElementsByTagName("input")[0].value;
@@ -51,23 +50,59 @@ function getQuote() {
     httpRequest.send(null);
     //Event handler for the onreadystatechange
     httpRequest.onreadystatechange = displayData;
+    //Refresh code sequence every 10 seconds
+    clearTimeout(updateQuote);
+    var updateQuote = setTimeout('getQuote()', 10000);
+}
+
+//Function event handler to see if data is given back
+function displayData() {
+    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+        var stockResults = httpRequest.responseText;
+        //console.log(stockResults);
+        var stockItems = stockResults.split(/,|\"/);
+        //console.log(stockItems);
+        for (var i = stockItems.length - 1; i >= 0; i--) {
+            if (stockItems[i] === "") {
+                stockItems.splice(i, 1);
+            }
+        }
+        //console.log(stockItems);
+        document.getElementById("ticker").innerHTML = stockItems[0];
+        document.getElementById("openingPrice").innerHTML = stockItems[6];
+        document.getElementById("lastTrade").innerHTML = stockItems[1];
+        document.getElementById("lastTradeDT").innerHTML = stockItems[2] + ", " + stockItems[3];
+        document.getElementById("change").innerHTML = stockItems[4];
+        document.getElementById("range").innerHTML = (stockItems[8] * 1).toFixed(2) + "&ndash;" + (stockItems[7] * 1).toFixed(2);
+        document.getElementById("volume").innerHTML = (stockItems[9] * 1).toLocaleString();
+
+    }
+
+}
+
+//Function to get better style into the stock data
+function formatTable() {
+    var rows = document.getElementsByTagName("tr");
+    for (var i = 0; i < rows.length; i++) {
+        rows[i].style.background = "#9FE098";
+    }
 }
 
 //Event handler to call function on subit event
 var form = document.getElementsByTagName("form")[0];
 if (form.addEventListener) {
     form.addEventListener("submit", stopSubmission, false);
+    window.addEventListener("load", formatTable, false);
     window.addEventListener("load", getQuote);
-}
-else if (form.attachEvent) {
+} else if (form.attachEvent) {
     form.attachEvent("onsubmit", stopSubmission);
+    window.attachEvent("onload", formatTable);
     window.attachEvent("onload", getQuote);
 }
 
 //Event handler for the page load event
 if (window.addEventListener) {
     window.addEventListener("load", getRequestObject, false);
-}
-else if (window.attachEvent) {
+} else if (window.attachEvent) {
     window.attachEvent("onload", getRequestObject);
 }
